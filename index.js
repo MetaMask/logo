11adf6ba805115cdda3c20078c8f9733d7212c67
kumavis <aaron@kumavis.me>
2016-01-09 18:04:10 -0800

    clean

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 module.exports = function(opts){
-	window.scene = new THREE.Scene();
+	window.scene = new THREE.Scene()
 
 	// CAMERA SETUP
-	window.camera = new THREE.PerspectiveCamera( 45, opts.width / opts.height, 1, 2000 );
-	camera.position.z = 400;
-	camera.lookAt(scene.position);
+	window.camera = new THREE.PerspectiveCamera( 45, opts.width / opts.height, 1, 2000 )
+	camera.position.z = 400
+	camera.lookAt(scene.position)
 
   // RENDERER OPTIONS
   window.renderer = new THREE.WebGLRenderer( {
     antialias: true,
     alpha: true,
-  });
+  })
   setSize(opts)
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.gammaInput = true;
-  renderer.gammaOutput = true;
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.cullFace = THREE.CullFaceBack;
+  renderer.setPixelRatio( window.devicePixelRatio )
+  renderer.gammaInput = true
+  renderer.gammaOutput = true
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.cullFace = THREE.CullFaceBack
 
   // DOM STUFF
-  var container = document.getElementById(opts.targetDivId);
-  container.appendChild( renderer.domElement );
+  var container = document.getElementById(opts.targetDivId)
+  container.appendChild( renderer.domElement )
 
   // MODEL LOADING:
-	var loader = new THREE.OBJMTLLoader();
+	var loader = new THREE.OBJMTLLoader()
 	loader.load( './fox.obj', './fox.mtl', function ( object ) {
-		window.object = object;
-		object.position = scene.position;
+		window.object = object
+		object.position = scene.position
 
-		object.rotation.x = 0;
-		object.rotation.y = 0;
-		object.rotation.z = 0;
+		object.rotation.x = 0
+		object.rotation.y = 0
+		object.rotation.z = 0
 
-		scene.add( object );
+		scene.add( object )
 
-		var ambiColor = "#FFFFFF";
-    var ambientLight = new THREE.AmbientLight(ambiColor);
-		scene.add( ambientLight );
-		animate();
-	});
+		var ambiColor = "#FFFFFF"
+    var ambientLight = new THREE.AmbientLight(ambiColor)
+		scene.add( ambientLight )
+		animate()
+	})
 
   // handle screen resize
 	window.addEventListener('resize', setSize.bind(null, opts))
@@ -58,46 +58,46 @@ module.exports = function(opts){
       lookAtMouse(object)
     } else {
       // drift left-right
-      object.rotation.y = 0.5 + (Math.sin(time/3000) * 0.2);
-      object.rotation.x = 0.1 + (Math.sin(time/3000) * 0.2);
-      object.rotation.z = -0.1 + (Math.sin(time/2000) * 0.03);
+      object.rotation.y = 0.5 + (Math.sin(time/3000) * 0.2)
+      object.rotation.x = 0.1 + (Math.sin(time/3000) * 0.2)
+      object.rotation.z = -0.1 + (Math.sin(time/2000) * 0.03)
     }
 
     // add other drift
-		requestAnimationFrame( animate );
-		render();
+		requestAnimationFrame( animate )
+		render()
 	}
 
   function lookAtMouse(object) {
-    var halfWidth = window.innerWidth/2;
-    var halfHeight = window.innerHeight/2;
-    var softness = 30;
+    var halfWidth = window.innerWidth/2
+    var halfHeight = window.innerHeight/2
+    var softness = 30
 
-    var x = (mouseX - halfWidth) / softness;
-    var y = (mouseY - halfHeight) / softness * -1;
-    var z = 10;
+    var x = (mouseX - halfWidth) / softness
+    var y = (mouseY - halfHeight) / softness * -1
+    var z = 10
 
-    var mousePos = new THREE.Vector3(x, y, z); 
+    var mousePos = new THREE.Vector3(x, y, z) 
 
     object.lookAt( mousePos )
   }
 
 	function render() {
-		// setSize(opts);
-		renderer.render( scene, camera );
+		// setSize(opts)
+		renderer.render( scene, camera )
 	}
 }
 
 function setSize(opts){
   if (!opts.pxNotRatio) {
-    var width = window.innerWidth * opts.width;
+    var width = window.innerWidth * opts.width
     width = Math.min(width, 800)
-    var height = width;
-    camera.aspect = height / width;
-    camera.updateProjectionMatrix();
+    var height = width
+    camera.aspect = height / width
+    camera.updateProjectionMatrix()
     console.log("SETTING SIZE:", width, height)
-    renderer.setSize(width, height);
+    renderer.setSize(width, height)
   } else {
-    renderer.setSize(opts.width, opts.height);
+    renderer.setSize(opts.width, opts.height)
   }
 }
--- sample.js
+++ sample.js
@@ -8,24 +8,21 @@ var viewer = require('./');
 //   targetDivId: 'modelDivLarge'
 // })
 //
-function detectmob() { 
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
-    return true;
-  }
- else {
-    return false;
-  }
+
+function detectMobile() {
+  return (
+      navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPad/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+  )
 }
 
 // To render as a ratio of the screen's width:
-if (detectmob()) { // Mobile devices with no mouse to follow
+if (detectMobile()) { // Mobile devices with no mouse to follow
   viewer({
     pxNotRatio: false, // Dictates whether width & height are px or multiplied
     width: 0.9,
