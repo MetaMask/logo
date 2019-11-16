0ca00656f6d2c4dc2aee312d1c21c7278df12ef0
Dan Finlay <dan@danfinlay.com>
2019-11-15 22:51:57 -0800

    Add save button and C to save

--- example/example.js
+++ example/example.js
@@ -1,19 +1,27 @@
 const colorSeed = Math.floor(Math.random() * 100000)
 
-document.addEventListener('keypress', function (event) {
-  if (event.keyCode === 99) { // the c key
+window.addEventListener('load', () => {
+  const saveButton = document.querySelector('button.save')
+  saveButton.addEventListener('click', saveImage)
+})
+
+document.addEventListener('keypress', (event) => {
+  if (event.keyCode === 99) { // the C key
+    saveImage()
+  }
+})
+
+function saveImage () {
     var svg = document.querySelector('svg')
     var inner = svg.innerHTML
     var head = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" '
     + '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> '
     + '<svg width="521px" height="521px" version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events">'
-    var foot = '</svg>'
+   var foot = '</svg>'
 
-     var full = head + inner + foot;
-
-     download(full, `custom-fox-${colorSeed}.svg`, 'image/svg+xml')
-  }
-})
+   var full = head + inner + foot;
+   download(full, `custom-fox-${colorSeed}.svg`, 'image/svg+xml')
+}
 
 var createViewer = require('../index')
 
