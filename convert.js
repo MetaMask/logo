be63178fe56d7cd868391094947db6c0a5ee4027
Mark Stacey <markjstacey@gmail.com>
2021-10-06 19:40:11 -0230

    Use `yargs` for `convert.js` CLI
    The `yargs` library is now used for the `convert.js` script. It allows
    customizing the input and output files, and it includes documentation
    for the command and all options.
    
    This was done in preparation for further customization to the convert
    process that will come in later PRs.
    
    Additionally, the output file is now formatted using `prettier` before
    being written to disk. This was to make updating the main `fox.json`
    easier (otherwise you'd need to run `yarn lint:fix` after each change).

--- convert.js
+++ convert.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs').promises;
 const OBJFile = require('obj-file-parser');
-
-// eslint-disable-next-line node/no-sync
-const obj = fs.readFileSync('./fox.obj').toString();
-// eslint-disable-next-line node/no-sync
-const mtlRaw = fs.readFileSync('./fox.mtl').toString('utf8');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const prettier = require('prettier');
 
 function parseMTL(mtl) {
   const output = {};
@@ -39,78 +36,118 @@ function parseMTL(mtl) {
   return output;
 }
 
-const mtl = parseMTL(mtlRaw);
+async function main() {
+  const { argv } = yargs(hideBin(process.argv))
+    .usage(
+      '$0 [options]',
+      'Convert Maya .obj and .mtl files into our JSON model format.',
+      (_yargs) =>
+        _yargs
+          .option('out', {
+            default: 'fox.json',
+            description: 'The output file path',
+            type: 'string',
+            normalize: true,
+          })
+          .option('obj', {
+            default: 'fox.obj',
+            description: 'The input OBJ file path',
+            type: 'string',
+            normalize: true,
+          })
+          .option('mtl', {
+            default: 'fox.mtl',
+            description: 'The input MTL file path',
+            type: 'string',
+            normalize: true,
+          }),
+    )
+    .version(false)
+    .strict();
 
-const objF = new OBJFile(obj);
+  const { out: outputFilename, obj: objFilename, mtl: mtlFilename } = argv;
 
-const data = objF.parse(objF);
+  const [objContents, mtlContents] = await Promise.all([
+    fs.readFile(objFilename, 'utf8'),
+    fs.readFile(mtlFilename, 'utf8'),
+  ]);
 
-const outpath = path.join(__dirname, 'fox.json');
+  const mtl = parseMTL(mtlContents);
+  const objFile = new OBJFile(objContents);
+  const data = objFile.parse(objFile);
 
-const output = {
-  positions: [],
-  chunks: [],
-};
+  const output = {
+    positions: [],
+    chunks: [],
+  };
 
-/*
+  /*
+  type colorValue = number; // 0-255
+  type positionId = number; // Index of that position
+  type CoordVal = number;
 
-type colorValue = number; // 0-255
-type positionId = number; // Index of that position
-type CoordVal = number;
+  type Position = [CoordVal, CoordVal, CoordVal];
+  type Face = [positionId, positionId, positionId];
 
-type Position = [CoordVal, CoordVal, CoordVal];
-type Face = [positionId, positionId, positionId];
-
-export type EfficientModel = {
-  positions: Array<Position>;
-  chunks: Array<{
-    color: [colorValue, colorValue, colorValue];
-    faces: Array<Face>;
-  }>;
-}
-
-*/
-const VI = 'vertexIndex';
-
-const model = data.models[0];
-model.vertices.forEach((v) => {
-  output.positions.push([v.x, v.y, v.z]);
-});
-
-for (const mtlKey of Object.keys(mtl)) {
-  const m = mtl[mtlKey];
-
-  if (m.Ka) {
-    const color = m.Kd.map(function (c) {
-      return Math.floor(255 * c);
-    });
-    m.Kd.forEach((c, i) => {
-      if (color[i] === 0) {
-        color[i] = Math.floor(255 * c);
-      }
-    });
-
-    const chunk = {
-      color,
-      faces: [],
-    };
-
-    model.faces.forEach((f) => {
-      // Only if this face matches the material!
-      if (f.material === mtlKey) {
-        chunk.faces.push([
-          f.vertices[0][VI] - 1,
-          f.vertices[1][VI] - 1,
-          f.vertices[2][VI] - 1,
-        ]);
-      }
-    });
-
-    output.chunks.push(chunk);
-  } else {
-    throw new Error(`Invalid MTL entry at key '${mtlKey}'`);
+  export type EfficientModel = {
+    positions: Array<Position>;
+    chunks: Array<{
+      color: [colorValue, colorValue, colorValue];
+      faces: Array<Face>;
+    }>;
   }
+
+  */
+  const VI = 'vertexIndex';
+
+  const model = data.models[0];
+  model.vertices.forEach((v) => {
+    output.positions.push([v.x, v.y, v.z]);
+  });
+
+  for (const mtlKey of Object.keys(mtl)) {
+    const m = mtl[mtlKey];
+
+    if (m.Ka) {
+      const color = m.Kd.map(function (c) {
+        return Math.floor(255 * c);
+      });
+      m.Kd.forEach((c, i) => {
+        if (color[i] === 0) {
+          color[i] = Math.floor(255 * c);
+        }
+      });
+
+      const chunk = {
+        color,
+        faces: [],
+      };
+
+      model.faces.forEach((f) => {
+        // Only if this face matches the material!
+        if (f.material === mtlKey) {
+          chunk.faces.push([
+            f.vertices[0][VI] - 1,
+            f.vertices[1][VI] - 1,
+            f.vertices[2][VI] - 1,
+          ]);
+        }
+      });
+
+      output.chunks.push(chunk);
+    } else {
+      throw new Error(`Invalid MTL entry at key '${mtlKey}'`);
+    }
+  }
+
+  const stringifiedOutput = JSON.stringify(output, null, 2);
+  const formattedOutput = prettier.format(stringifiedOutput, {
+    filepath: outputFilename,
+  });
+  await fs.writeFile(outputFilename, formattedOutput);
 }
 
-// eslint-disable-next-line node/no-sync
-fs.writeFileSync(outpath, JSON.stringify(output, null, 2));
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
