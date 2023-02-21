5135c8738d52e19141eca7ef4d65e986fc9bc942
Albert Olivé <albertolivecorbella@gmail.com>
2023-02-21 16:41:02 +0100

    feat: added mmi logo for the metamerge project

---
+++ demo/src/mmi/mmi.js
@@ -0,0 +1,21 @@
+const copy = require('copy-to-clipboard');
+const createViewer = require('../../..');
+const { svgElementToSvgImageContent } = require('../../../util');
+const meshJson = require('./mmi-fox.json');
+
+document.addEventListener('keypress', function (event) {
+  if (event.keyCode === 99) {
+    // the c key
+    const svg = document.querySelector('svg');
+    const content = svgElementToSvgImageContent(svg);
+    copy(content);
+  }
+});
+
+createViewer({
+  width: 0.4,
+  height: 0.4,
+  followMouse: true,
+  followMotion: true,
+  meshJson,
+});
